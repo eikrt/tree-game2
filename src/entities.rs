f9bef350c38b7655fc786667810fc2a2d2650da8
Eino Korte <e.i.korte@gmail.com>
2022-12-18 00:45:37 +0200

    improved tree algo

--- src/entities.rs
+++ src/entities.rs
@@ -23,70 +23,15 @@ impl Entity {
     }
     pub fn tick(&mut self, delta: u64) {
         self.lifetime += delta;
-        self.live(); 
-    }
-    pub fn live(&mut self) {
-        if self.lifetime > 150000 {
-            return;
-        }
-        match &self.entity_type {
-            EntityType::Tree => {
-                if self.lifetime % 4000 == 0 {
-                    self.grow();
-                }
-                if self.lifetime % 10000 == 0 {
-                    self.branch();
-                }
-            }
-            _ => {}
-        }
-    }
-    pub fn grow(&mut self) {
         for line in &mut self.mesh.lines {
-
-            if line.branches > 1 && !line.main_branch {
-                continue;
-            }
-            if line.line_type == LineType::Leaf && line.grows > 3 {
-                continue;
-            }
-            line.grow();
-        }
-    }
-    pub fn branch(&mut self) {
-        let mut rng = rand::thread_rng();
-        let mut push_lines = Vec::new();
-        for line in &mut self.mesh.lines {
-            let x: f32 = rng.gen_range(-4.0..4.0); // generates a float between 0 and 1
-            let y: f32 = rng.gen_range(0.0..4.0); // generates a float between 0 and 1
-            line.branch();
-            let mut color = Color::RGB(80,65,40);
-            let mut line_type = LineType::Branch;
-            if line.branches > 1 && !line.main_branch {
-                color = Color::RGB(0,255,0);
-                line_type = LineType::Leaf;
-            }
-            if line.branches > 2 && !line.main_branch {
-                continue;
-            }
-            push_lines.push(Line::new(
-                false,
-                line_type,
-                color,
-                (
-                    (line.points.1 .0, line.points.1 .1),
-                    (line.points.1 .0 - x, line.points.1 .1 - y),
-                ),
-            ));
-        }
-        for mut line in push_lines {
-            self.mesh.lines.push(line.clone());
+            line.tick(delta);
         }
     }
     pub fn draw(&self, canvas: &mut WindowCanvas) {
-        for line in self.mesh.lines.iter() {
+        for line in &self.mesh.lines {
             canvas.set_draw_color(line.color);
-            canvas.draw_line(line.get_points().0, line.get_points().1);
+            line.draw(canvas);
         }
+
     }
 }
--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,7 @@
-use sdl2::rect::Point;
+use rand::Rng;
 use sdl2::pixels::Color;
-
+use sdl2::rect::Point;
+use sdl2::render::WindowCanvas;
 #[derive(Clone, PartialEq)]
 pub enum LineType {
     Branch,
@@ -9,35 +10,90 @@ pub enum LineType {
 }
 #[derive(Clone)]
 pub struct Line {
-    pub points: ((f32,f32), (f32,f32)),
-    pub color: Color, 
+    pub points: ((f32, f32), (f32, f32)),
+    pub color: Color,
     pub grows: u32,
-    pub branches: u32,
     pub main_branch: bool,
     pub line_type: LineType,
+    pub leafs: Vec<Line>,
+    pub lifetime: u64,
 }
 impl Line {
-    pub fn new(main_branch: bool, line_type: LineType,color: Color, points: ((f32,f32), (f32,f32))) -> Line {
+    pub fn new(
+        main_branch: bool,
+        line_type: LineType,
+        color: Color,
+        points: ((f32, f32), (f32, f32)),
+    ) -> Line {
         Line {
-            color: color, 
+            color: color,
             points: points,
             grows: 0,
-            branches: 0,
             line_type: line_type,
             main_branch: main_branch,
-        }  
+            leafs: Vec::new(),
+            lifetime: 0,
+        }
+    }
+    pub fn tick(&mut self, delta: u64) {
+        self.lifetime += delta;
+
+        for leaf in &mut self.leafs {
+            leaf.tick(delta);
+        }
+        match &self.line_type {
+            LineType::Branch => {
+                if self.lifetime % 2000 == 0 {
+                    self.grow();
+                }
+                if self.lifetime % 6000 == 0 {
+                    self.branch();
+                }
+            }
+            _ => {}
+        }
     }
     pub fn grow(&mut self) {
         self.grows += 1;
-        let grow_dir = ((self.points.0.1 - self.points.1.1)).atan2((self.points.1.0 - self.points.0.0)) + 3.14 / 2.0;
-        self.points.1.0 += grow_dir.sin() * 2.0;
-        self.points.1.1 += grow_dir.cos() * 2.0;
+        let grow_dir = (self.points.0 .1 - self.points.1 .1)
+            .atan2((self.points.1 .0 - self.points.0 .0))
+            + 3.14 / 2.0;
+        self.points.1 .0 += grow_dir.sin() * 2.0;
+        self.points.1 .1 += grow_dir.cos() * 2.0;
     }
     pub fn branch(&mut self) {
-        self.branches += 1;
+        let mut color = Color::RGB(80, 65, 40);
+        let mut line_type = LineType::Branch;
+        if self.leafs.len() > 1 {
+            color = Color::RGB(0, 255, 0);
+            line_type = LineType::Leaf;
+        }
+
+        let mut rng = rand::thread_rng();
+        let x: f32 = rng.gen_range(-4.0..4.0); // generates a float between 0 and 1
+        let y: f32 = rng.gen_range(0.0..4.0); // generates a float between 0 and 1
+        self.leafs.push(Line::new(
+            false,
+            line_type,
+            color,
+            (
+                (self.points.1 .0, self.points.1 .1),
+                (self.points.1 .0 - x, self.points.1 .1 - y),
+            ),
+        ));
     }
     pub fn get_points(&self) -> (Point, Point) {
-        (Point::new(self.points.0.0 as i32, self.points.0.1 as i32), Point::new(self.points.1.0 as i32, self.points.1.1 as i32))
+        (
+            Point::new(self.points.0 .0 as i32, self.points.0 .1 as i32),
+            Point::new(self.points.1 .0 as i32, self.points.1 .1 as i32),
+        )
+    }
+
+    pub fn draw(&self, canvas: &mut WindowCanvas) {
+        canvas.draw_line(self.get_points().0, self.get_points().1);
+        for leaf in &self.leafs {
+            leaf.draw(canvas);
+        }
     }
 }
 pub struct Mesh {
